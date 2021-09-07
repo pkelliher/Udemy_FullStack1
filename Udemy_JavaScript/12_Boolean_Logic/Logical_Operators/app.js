9f9339efa45987aef1600be346de3e1ed147d3e8
pkelliher <pkelliher74@gmail.com>
2021-09-06 20:42:38 -0700

    Logical Operators

--- Udemy_JavaScript/12_Boolean_Logic/Logical_Operators/app.js
+++ Udemy_JavaScript/12_Boolean_Logic/Logical_Operators/app.js
@@ -1,31 +1,33 @@
+// Logical Operators
+
 // =====================
 // LOGICAL AND OPERATOR
 // =====================
 const password = prompt("Enter your password");
-if (password.length >= 6 && password.indexOf(' ') === -1) {
-    console.log("VALID PASSWORD!")
+if (password.length >= 6 && password.indexOf(" ") === -1) {
+  console.log("VALID PASSWORD!");
 } else {
-    console.log("INCORRECT FORMAT FOR PASSWORD!")
+  console.log("INCORRECT FORMAT FOR PASSWORD!");
 }
 
 // =====================
 // REVISITING AGE EXAMPLE
 // =====================
 
-// 0-5 free 
-// 5-10 $10 
+// 0-5 free
+// 5-10 $10
 // 10-65 $20
 // 65+ free
 
 const age = 100;
 if ((age >= 0 && age < 5) || age >= 65) {
-    console.log("FREE");
+  console.log("FREE");
 } else if (age >= 5 && age < 10) {
-    console.log("$10")
+  console.log("$10");
 } else if (age >= 10 && age < 65) {
-    console.log("$20")
+  console.log("$20");
 } else {
-    console.log("INVALID AGE!")
+  console.log("INVALID AGE!");
 }
 
 // let firstName = prompt("enter your first name");
@@ -37,9 +39,6 @@ if ((age >= 0 && age < 5) || age >= 65) {
 // COMBINING && and ||
 // =====================
 const age = 8;
-if (!(age >= 0 && age < 5 || age >= 65)) {
-    console.log("YOU ARE NOT A BABY OR A SENIOR!")
+if (!((age >= 0 && age < 5) || age >= 65)) {
+  console.log("YOU ARE NOT A BABY OR A SENIOR!");
 }
-
-
-
