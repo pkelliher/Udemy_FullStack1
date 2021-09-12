a6cbcb199913dd8c39279c47767e7d0ede9aa1e9
pkelliher <pkelliher74@gmail.com>
2021-09-12 16:02:02 -0700

    todos todos for loop

--- Udemy_JavaScript/Loops_CODE/TodoListProject/todos.js
+++ Udemy_JavaScript/Loops_CODE/TodoListProject/todos.js
@@ -26,6 +26,15 @@
 // console.log("OK QUIT THE APP!");
 
 let input = prompt("what would you like to do?");
-while (input !== "quit") {
-  input = prompt("what would you like to do?");
+const todos = ["Collect Chicket Eggs", "Clean Litter Box"];
+while (input !== "quit" && input !== "q") {
+  if (input === "list") {
+    console.log("***********");
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`);
+    }
+    console.log("***********");
+  }
+  input = prompt("What would you like to do?");
 }
+console.log("OK QUIT THE APP!");
