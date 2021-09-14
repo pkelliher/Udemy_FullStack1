ca70d43503665e752dc29bac6eb19463197b9eb5
pkelliher <pkelliher74@gmail.com>
2021-09-13 19:56:19 -0700

    Function Examples

--- Udemy_JavaScript/Functions_Intro_CODE/app.js
+++ Udemy_JavaScript/Functions_Intro_CODE/app.js
@@ -1,32 +1,30 @@
+// Function examples
+
 function singSong() {
-    console.log("DO");
-    console.log("RE");
-    console.log("MI");
+  console.log("DO");
+  console.log("RE");
+  console.log("MI");
 }
 
 function greet(firstName, lastName) {
-    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+  console.log(`Hey there, ${firstName} ${lastName[0]}.`);
 }
 
 function repeat(str, numTimes) {
-    let result = '';
-    for (let i = 0; i < numTimes; i++) {
-        result += str;
-    }
-    console.log(result);
+  let result = "";
+  for (let i = 0; i < numTimes; i++) {
+    result += str;
+  }
+  console.log(result);
 }
 
 function add(x, y) {
-    if (typeof x !== 'number' || typeof y !== 'number') {
-        return false;
-    }
-    return x + y;
+  if (typeof x !== "number" || typeof y !== "number") {
+    return false;
+  }
+  return x + y;
 }
 
-
-
-
-
 // singSong()
 // singSong()
 // singSong()
@@ -38,4 +36,3 @@ function add(x, y) {
 // singSong()
 // singSong()
 // singSong()
-
