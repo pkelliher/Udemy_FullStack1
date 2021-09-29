0082b0c348404fc2369811ed7b02631f328ae34a
pkelliher <pkelliher74@gmail.com>
2021-09-28 19:59:15 -0700

    The Events Object

--- Udemy_DOM/DOM_Events_CODE/TheEventObject/app.js
+++ Udemy_DOM/DOM_Events_CODE/TheEventObject/app.js
@@ -1,6 +1,7 @@
-document.querySelector('button').addEventListener('click', function (evt) {
-    console.log(evt)
-})
+// The Events Object
+document.querySelector("button").addEventListener("click", function (evt) {
+  console.log(evt);
+});
 
 // const input = document.querySelector('input');
 // input.addEventListener('keydown', function (e) {
@@ -11,21 +12,21 @@ document.querySelector('button').addEventListener('click', function (evt) {
 //     console.log("KEYUP")
 // })
 
-window.addEventListener('keydown', function (e) {
-    switch (e.code) {
-        case 'ArrowUp':
-            console.log("UP!");
-            break;
-        case 'ArrowDown':
-            console.log("DOWN!");
-            break;
-        case 'ArrowLeft':
-            console.log("LEFT!");
-            break;
-        case 'ArrowRight':
-            console.log("RIGHT!");
-            break
-        default:
-            console.log("IGNORED!")
-    }
-})
+window.addEventListener("keydown", function (e) {
+  switch (e.code) {
+    case "ArrowUp":
+      console.log("UP!");
+      break;
+    case "ArrowDown":
+      console.log("DOWN!");
+      break;
+    case "ArrowLeft":
+      console.log("LEFT!");
+      break;
+    case "ArrowRight":
+      console.log("RIGHT!");
+      break;
+    default:
+      console.log("IGNORED!");
+  }
+});
