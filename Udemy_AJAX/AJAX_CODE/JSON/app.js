bbc33e7aa4414225feeb4d7d01b5e2e97f69d962
pkelliher <pkelliher74@gmail.com>
2021-10-06 18:16:36 -0700

    comment Axios

--- Udemy_AJAX/AJAX_CODE/JSON/app.js
+++ Udemy_AJAX/AJAX_CODE/JSON/app.js
@@ -1,7 +1,5 @@
 //THIS IS A STRING OF JSON (NOT AN OBJECT)
-const data = `{"ticker":{"base":"BTC","target":"USD","price":"11288.49813464","volume":"91769.69699773","change":"-46.29462447"},"timestamp":1596510482,"success":true,"error":""}`
+const data = `{"ticker":{"base":"BTC","target":"USD","price":"11288.49813464","volume":"91769.69699773","change":"-46.29462447"},"timestamp":1596510482,"success":true,"error":""}`;
 
-// THIS IS A JS OBJECT
+// THIS IS A JS OBJECT example
 const parsedData = JSON.parse(data);
-
-
